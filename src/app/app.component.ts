34ddd871880f61caa11ac2f56c39339e06ae4a04
Katya <ekaterina.leo.nikiforova@gmail.com>
2021-03-31 23:32:41 +0300

    ngClass directive

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pages';
+  currentPage = 0;
+
+  images = [
+    {
+      title: 'At the Beach',
+      url: 'https://images.unsplash.com/photo-1519046904884-53103b34b206?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mnx8YmVhY2h8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60'
+    },
+    {
+      title: 'In the forest',
+      url: 'https://images.unsplash.com/photo-1441974231531-c6227db76b6e?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NHx8Zm9yZXN0fGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60'
+    },
+    {
+      title: 'In the City',
+      url: 'https://images.unsplash.com/photo-1519501025264-65ba15a82390?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTR8fGNpdHl8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60'
+    },
+    {
+      title: 'Up the mountains',
+      url: 'https://images.unsplash.com/photo-1609849928935-3518b1e78e8c?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mnx8bW91bmF0aW5zfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60'
+    }
+  ]
 }
