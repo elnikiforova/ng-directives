eedc5e086aeee178f27caa14814de68a92d4a071
Katya <ekaterina.leo.nikiforova@gmail.com>
2021-04-02 21:44:03 +0300

    showing images on page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -24,6 +24,70 @@ export class AppComponent {
     {
       title: 'Up the mountains',
       url: 'https://images.unsplash.com/photo-1609849928935-3518b1e78e8c?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mnx8bW91bmF0aW5zfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60'
+    },
+    {
+      title: 'At the Beach',
+      url: 'https://images.unsplash.com/photo-1519046904884-53103b34b206?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mnx8YmVhY2h8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60'
+    },
+    {
+      title: 'In the forest',
+      url: 'https://images.unsplash.com/photo-1441974231531-c6227db76b6e?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NHx8Zm9yZXN0fGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60'
+    },
+    {
+      title: 'In the City',
+      url: 'https://images.unsplash.com/photo-1519501025264-65ba15a82390?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTR8fGNpdHl8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60'
+    },
+    {
+      title: 'Up the mountains',
+      url: 'https://images.unsplash.com/photo-1609849928935-3518b1e78e8c?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mnx8bW91bmF0aW5zfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60'
+    },
+    {
+      title: 'At the Beach',
+      url: 'https://images.unsplash.com/photo-1519046904884-53103b34b206?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mnx8YmVhY2h8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60'
+    },
+    {
+      title: 'In the forest',
+      url: 'https://images.unsplash.com/photo-1441974231531-c6227db76b6e?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NHx8Zm9yZXN0fGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60'
+    },
+    {
+      title: 'In the City',
+      url: 'https://images.unsplash.com/photo-1519501025264-65ba15a82390?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTR8fGNpdHl8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60'
+    },
+    {
+      title: 'Up the mountains',
+      url: 'https://images.unsplash.com/photo-1609849928935-3518b1e78e8c?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mnx8bW91bmF0aW5zfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60'
+    },
+    {
+      title: 'At the Beach',
+      url: 'https://images.unsplash.com/photo-1519046904884-53103b34b206?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mnx8YmVhY2h8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60'
+    },
+    {
+      title: 'In the forest',
+      url: 'https://images.unsplash.com/photo-1441974231531-c6227db76b6e?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NHx8Zm9yZXN0fGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60'
+    },
+    {
+      title: 'In the City',
+      url: 'https://images.unsplash.com/photo-1519501025264-65ba15a82390?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTR8fGNpdHl8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60'
+    },
+    {
+      title: 'Up the mountains',
+      url: 'https://images.unsplash.com/photo-1609849928935-3518b1e78e8c?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mnx8bW91bmF0aW5zfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60'
+    },
+    {
+      title: 'At the Beach',
+      url: 'https://images.unsplash.com/photo-1519046904884-53103b34b206?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mnx8YmVhY2h8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60'
+    },
+    {
+      title: 'In the forest',
+      url: 'https://images.unsplash.com/photo-1441974231531-c6227db76b6e?ixid=MXwxMjA3fDB8MHxzZWFyY2h8NHx8Zm9yZXN0fGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60'
+    },
+    {
+      title: 'In the City',
+      url: 'https://images.unsplash.com/photo-1519501025264-65ba15a82390?ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTR8fGNpdHl8ZW58MHx8MHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60'
+    },
+    {
+      title: 'Up the mountains',
+      url: 'https://images.unsplash.com/photo-1609849928935-3518b1e78e8c?ixid=MXwxMjA3fDB8MHxzZWFyY2h8Mnx8bW91bmF0aW5zfGVufDB8fDB8&ixlib=rb-1.2.1&auto=format&fit=crop&w=900&q=60'
     }
   ]
 }
